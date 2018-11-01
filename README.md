c36a4a0417abeacce71b9007661d3c59b5c2a114
fanLiUB <kripkeli@bu.edu>
2018-11-01 10:28:18 +0800

    first two parts completed

--- README.md
+++
@@ -1,3 +0,0 @@
-# sphinx-epson
-
-sphinx
--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ author = 'Fan Li, Yuxin Gao'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -111,7 +111,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'epsondoc'
+htmlhelp_basename = 'EPSONdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -138,7 +138,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'epson.tex', 'epson Documentation',
+    (master_doc, 'EPSON.tex', 'EPSON Documentation',
      'Fan Li, Yuxin Gao', 'manual'),
 ]
 
@@ -148,7 +148,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'epson', 'epson Documentation',
+    (master_doc, 'epson', 'EPSON Documentation',
      [author], 1)
 ]
 
@@ -159,8 +159,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'epson', 'epson Documentation',
-     author, 'epson', 'One line description of project.',
+    (master_doc, 'EPSON', 'EPSON Documentation',
+     author, 'EPSON', 'One line description of project.',
      'Miscellaneous'),
 ]
 
